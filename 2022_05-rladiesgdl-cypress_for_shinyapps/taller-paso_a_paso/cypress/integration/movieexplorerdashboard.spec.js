deb6c024b186ba6c20f6c2d854a6be4058af4448
semiramisCJ <semiramis.cj@gmail.com>
2022-05-28 03:58:00 -0500

    Initial tests for movieexplorer shiny app

---
+++ 2022_05-rladiesgdl-cypress_for_shinyapps/taller-paso_a_paso/cypress/integration/movieexplorerdashboard.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+let mode = process.env.MODE
+
+let url = ""
+if (mode === "compose") {
+  url = "http://movieexplorer:8080/"
+} else {
+  url = "http://localhost:8080/"
+}
+
+describe("some checks to movie-explorer app", () => {
+  before(() => {
+    cy.visit(url, { timeout: 30000 })
+  })
+
+  it("we can filter by director", () => {
+    cy.get("#director").type("Hayao")
+    cy.get("#n_movies").should("contain", 4)
+    cy.get("#plot1").should("be.visible").and("not.have.class", "animating")
+  })
+
+
+
+})
