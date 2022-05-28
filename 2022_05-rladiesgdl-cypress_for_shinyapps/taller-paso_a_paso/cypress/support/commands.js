24a78b56a3160fad4bc25c34627632b5221ac897
semiramisCJ <semiramis.cj@gmail.com>
2022-05-28 05:34:00 -0500

    Deleted examples

--- 2022_05-rladiesgdl-cypress_for_shinyapps/taller-paso_a_paso/cypress/support/commands.js
+++ 2022_05-rladiesgdl-cypress_for_shinyapps/taller-paso_a_paso/cypress/support/commands.js
@@ -9,20 +9,7 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 
 Cypress.Commands.add("verifyOutput", (expected) => {
     cy.get("#n_movies").should("contain", expected)
